b3043d9789fe959dda8e8c30ff4595eb1e3b134c
Stefan Stefanov <stefanov.sm@abv.bg>
2018-09-27 01:04:57 +0300

    Add files via upload

--- examples/restclient/service.invoker.php
+++ examples/restclient/service.invoker.php
@@ -1,7 +1,7 @@
 <?php
 function http_post_json($url, $content, $authorization)
 {
-	$content_length = strlen($content);
+    $content_length = strlen($content);
     $headers = <<<HEADERS
 Content-type: application/json; charset=utf-8
 Content-Length: {$content_length}
@@ -13,18 +13,17 @@ HEADERS;
     $retval = [];
     if (trim($url))
     {
-    	$retval['value'] = @ file_get_contents($url, FALSE, $context);
-    	$retval['http_status'] = $http_response_header[0];
+        $retval['value'] = @ file_get_contents($url, FALSE, $context);
+        $retval['http_status'] = $http_response_header[0];
     }
     else
     {
-    	$retval['value'] = 'No URL given';
-    	$retval['http_status'] = 'No HTTP status';
+        $retval['value'] = 'No URL given';
+        $retval['http_status'] = 'No HTTP status';
     }
-    
+
     return (object)$retval;
 }
-
 
 $timeIn = microtime(true);
 $response = http_post_json($_POST['inputURL'], $_POST['inputData'],trim($_POST['inputToken']));
@@ -34,19 +33,19 @@ $msPassed = round($timeOut-$timeIn, 3) * 1000;
 echo <<<HTML
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8"/>
+    <meta charset="utf-8"/>
 </head>
 <body>
-	<table style="width:100%">
-		<tr>
-			<td style="color:red; width: 80%">{$response->http_status}, execution time {$msPassed} ms</td>
-			<td style="width: 20%" align="right"><input type="button" value="Clear" onclick="document.location.href='about:blank'" ></td>
-		</tr>
-	</table>
-	<hr>
-	<pre>
+    <table style="width:100%">
+        <tr>
+            <td style="color:red; width: 80%">{$response->http_status}, execution time {$msPassed} ms</td>
+            <td style="width: 20%" align="right"><input type="button" value="Clear" onclick="document.location.href='about:blank'" ></td>
+        </tr>
+    </table>
+    <hr>
+    <pre>
 {$response->value}
-	</pre>
+    </pre>
 </body>
 </html>
 HTML;
