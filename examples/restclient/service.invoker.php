3c23fa7829f2d24f88aff07c7caa341571daaea0
Stefan Stefanov <stefanov.sm@abv.bg>
2018-09-27 14:13:21 +0300

    Add files via upload

--- examples/restclient/service.invoker.php
+++ examples/restclient/service.invoker.php
@@ -1,36 +1,38 @@
 <?php
 function http_post_json($url, $content, $authorization)
 {
-    $content_length = strlen($content);
-    $headers = <<<HEADERS
-Content-type: application/json; charset=utf-8
-Content-Length: {$content_length}
-Authorization: {$authorization}
-HEADERS;
-
-    $raw_context = ['http' => ['method' => 'POST', 'header' => $headers, 'ignore_errors' => TRUE, 'content' => $content]];
+	$headers = "Content-type: application/json; charset=utf-8\r\nAuthorization: {$authorization}";
+    $raw_context = 
+    [
+    	'http' => ['method' => 'POST', 'header' => $headers, 'ignore_errors' => TRUE, 'content' => $content] 
+    	,'ssl' => ['verify_peer' => false, 'verify_peer_name' => false]
+    ];
     $context = stream_context_create($raw_context);
-    $retval = [];
-    if (trim($url))
+    $retval = ['http_status' => 'No HTTP status'];
+    if (preg_match('/^https?:\/\/[^\s\/\$\.\?\#\(\)]*(\.[^\s]*)*$/', $url))
     {
         $retval['value'] = @ file_get_contents($url, FALSE, $context);
-        $retval['http_status'] = $http_response_header[0];
+        if (!$retval['value'])
+        {
+	        $retval['value'] = 'No response, probably bad URL given';
+	    }
+	    else
+	    {
+        	$retval['http_status'] = $http_response_header[0];
+        }
     }
     else
     {
-        $retval['value'] = 'No URL given';
-        $retval['http_status'] = 'No HTTP status';
+        $retval['value'] = 'No valid URL given';
     }
-
-    return (object)$retval;
+    return $retval;
 }
 
 $timeIn = microtime(true);
-$response = http_post_json($_POST['inputURL'], $_POST['inputData'],trim($_POST['inputToken']));
+$response = http_post_json(trim($_POST['inputURL']), $_POST['inputData'], trim($_POST['inputToken']));
 $timeOut = microtime(true);
 $msPassed = round($timeOut-$timeIn, 3) * 1000;
-
-echo <<<HTML
+?>
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8"/>
@@ -38,14 +40,13 @@ echo <<<HTML
 <body>
     <table style="width:100%">
         <tr>
-            <td style="color:red; width: 80%">{$response->http_status}, execution time {$msPassed} ms</td>
+            <td style="color:red; width: 80%"><?php echo $response['http_status']; ?>, execution time <?php echo $msPassed; ?> ms</td>
             <td style="width: 20%" align="right"><input type="button" value="Clear" onclick="document.location.href='about:blank'" ></td>
         </tr>
     </table>
     <hr>
     <pre>
-{$response->value}
+<?php echo $response['value']; ?>
     </pre>
 </body>
 </html>
-HTML;
