a6300d340d4198859ca216b1d82fab84973a0777
Stefan Stefanov <stefanov.sm@abv.bg>
2018-12-17 23:04:43 +0200

    POST request data made optional
    If POST data checkbox is not selected then a GET request is issued

--- examples/restclient/service.invoker.php
+++ examples/restclient/service.invoker.php
@@ -6,7 +6,36 @@ function http_post_json($url, $content, $authorization)
     $raw_context = 
     [
         'http' => ['method' => 'POST', 'header' => $headers, 'ignore_errors' => TRUE, 'content' => $content] 
-        // ,'ssl' => ['verify_peer' => false, 'verify_peer_name' => false]
+        ,'ssl' => ['verify_peer' => false, 'verify_peer_name' => false]
+    ];
+    $context = stream_context_create($raw_context);
+    $retval = ['http_status' => 'No HTTP status'];
+    if (preg_match('/^https?:\/\/[^\s\/\$\.\?\#\(\)]*(\.[^\s]*)*$/', $url))
+    {
+        $retval['value'] = @ file_get_contents($url, FALSE, $context);
+        if (!$retval['value'])
+        {
+            $retval['value'] = 'No response, probably bad URL given';
+        }
+        else
+        {
+            $retval['http_status'] = $http_response_header[0];
+        }
+    }
+    else
+    {
+        $retval['value'] = 'No valid URL given';
+    }
+    return $retval;
+}
+
+function http_get_json($url, $authorization)
+{
+    $headers = "Authorization: {$authorization}";
+    $raw_context = 
+    [
+        'http' => ['method' => 'GET', 'header' => $headers, 'ignore_errors' => TRUE] 
+        ,'ssl' => ['verify_peer' => false, 'verify_peer_name' => false]
     ];
     $context = stream_context_create($raw_context);
     $retval = ['http_status' => 'No HTTP status'];
@@ -30,22 +59,38 @@ function http_post_json($url, $content, $authorization)
 }
 
 $timeIn = microtime(true);
-$response = http_post_json(trim($_POST['inputURL']), $_POST['inputData'], trim($_POST['inputToken']));
+
+if ($_POST['isPost'] === 'on')
+{
+	$response = http_post_json(trim($_POST['inputURL']), $_POST['inputData'], trim($_POST['inputToken']));
+	$method = 'POST: ';
+}
+else	
+{
+	$response = http_get_json(trim($_POST['inputURL']), trim($_POST['inputToken']));
+	$method = 'GET: ';
+}
 $timeOut = microtime(true);
 $msPassed = round($timeOut-$timeIn, 3) * 1000;
+
+header('Cache-Control: no-cache, no-store, must-revalidate');
+
 ?>
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8"/>
+    <meta http-equiv='cache-control' content='no-cache'>
+	<meta http-equiv='expires' content='0'>
+	<meta http-equiv='pragma' content='no-cache'>
 </head>
 <body>
     <table style="width:100%">
         <tr>
-            <td style="color:red; width: 80%"><?php echo $response['http_status']; ?>, execution time <?php echo $msPassed; ?> ms</td>
-            <td style="width: 20%" align="right"><input type="button" value="Clear" onclick="document.location.href='about:blank'"/></td>
+            <td style="color:red; width: 80%"><?php echo $method.$response['http_status']; ?>, execution time <?php echo $msPassed; ?> ms </td>
+            <td style="width: 20%" align="right"><input type="button" value="Clear" onclick="document.location.href='about:blank'" ></td>
         </tr>
     </table>
-    <hr style="border-top: 1px solid black;"/>
+    <hr>
     <pre>
 <?php echo $response['value']; ?>
     </pre>
