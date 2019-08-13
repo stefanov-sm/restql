45d210d0a222ebaa50680a295340acef9e832bdb
Stefan Stefanov <stefanov.sm@abv.bg>
2019-08-13 09:23:14 +0300

    Update restql.php

--- restql.php
+++ restql.php
@@ -1,4 +1,4 @@
 <?php 
- require('include/restql.class.php');
+ require(__DIR__.DIRECTORY_SEPARATOR.'include'.DIRECTORY_SEPARATOR.'restql.class.php');
  $svc = new Restql(__DIR__.DIRECTORY_SEPARATOR.'include', __DIR__.DIRECTORY_SEPARATOR.'services');
  $svc -> handle();
